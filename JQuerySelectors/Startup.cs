0f3d2909c23e6e1a1d4a1cf3736617d4f4f56ed6
Pei <jzha471@aucklanduni.ac.nz>
2017-08-02 18:19:17 +1200

    Add project files.

---
+++ JQuerySelectors/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JQuerySelectors.Startup))]
+namespace JQuerySelectors
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
