4baf17e09128c1b63572f8b8aacfec478f4b1898
Pei <jzha471@aucklanduni.ac.nz>
2017-08-05 02:30:48 +1200

    3. Interacting with the DOM ( a start project , jQueryDom )

---
+++ jQueryDom/jQueryDom/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jQueryDom.Startup))]
+namespace jQueryDom
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
