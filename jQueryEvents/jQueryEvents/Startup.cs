543959cb509b8d4664dba3d9e3954fffc8202ea5
Pei <jzha471@aucklanduni.ac.nz>
2017-08-06 11:38:21 +1200

    3. Handling events ( a start page )

---
+++ jQueryEvents/jQueryEvents/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jQueryEvents.Startup))]
+namespace jQueryEvents
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
